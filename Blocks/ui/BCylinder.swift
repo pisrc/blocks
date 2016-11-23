759719b03f39af9186ef3c4741044e774a3a5dfb
pi <pimail31@gmail.com>
2016-11-23 11:50:02 +0900

    cylinder 작업중

---
+++ Blocks/ui/BCylinder.swift
@@ -0,0 +1,21 @@
+//
+//  BCylinder.swift
+//  Blocks
+//
+//  Created by ryan on 23/11/2016.
+//  Copyright © 2016 pi. All rights reserved.
+//
+
+import UIKit
+
+class BCylinderViewController: UIViewController {
+    
+    var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView = UIScrollView()
+        
+    }
+}
