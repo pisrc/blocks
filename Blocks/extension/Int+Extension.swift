11d280f9a2804530335bca03eabfa5de26570113
pi <pimail31@gmail.com>
2016-12-15 17:10:36 +0900

    rotate 추가

--- Blocks/extension/Int+Extension.swift
+++ Blocks/extension/Int+Extension.swift
@@ -19,4 +19,20 @@ extension Int {
     public func rotateBack(max: Int) -> Int {
         return (0 <= self - 1) ? self - 1 : max
     }
+    
+    // ring 형 순열 버퍼에서 에서 step 만큼 증가시켰을때 index 값을 반환
+    // ex: 4.rotate(next: -1, length: 3) -> 0
+    // length:3 에의한 [0,1,2] 순열중 4번째 index는 1, 0에서 -1 step 은 0
+    public func rotate(next step: Int, length: Int) -> Int {
+        
+        guard self < length else {
+            return 0.rotate(next: self + step, length: length)
+        }
+        
+        let nextIdx = (self + step) % length
+        if 0 <= nextIdx {
+            return nextIdx
+        }
+        return length + nextIdx
+    }
 }
