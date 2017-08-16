68c4533683887716532f7b7eb4cfafb3c7fe0c85
pi <pimail31@gmail.com>
2017-08-16 17:17:43 +0900

    overFullScreen 제거

--- Blocks/segue/Segue.swift
+++ Blocks/segue/Segue.swift
@@ -107,7 +107,6 @@ public struct Segue {
             }
             
         case .presentModally(let animated):
-            destination.modalPresentationStyle = .overFullScreen
             segue = PresentModallySegue(identifier: nil, source: source, destination: destination)
             if var segue = segue as? AnimatedSettable {
                 segue.animated = animated
